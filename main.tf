8c587a2250142b072b98c032b6f6d8fa237c7527
Marc-Antoine Abou Jaoude <marc.abou.jaoude@hotmail.com>
2022-09-30 20:20:21 -0400

    app deployment using docker

--- main.tf
+++ main.tf
@@ -12,13 +12,13 @@ terraform {
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = "ASIAZ3DCZEO2EZPJDXHM"
-  secret_key = "aXwrP6RvuR/Jv8o5NrjHjQZUpj/czepKxDfCTuGZ"
-  token      = "FwoGZXIvYXdzEBoaDIPjgZSN8hhEskto9CLJAQ9H7DUUzFgvE/b8S+RV65wR2dLbqqw5bBEcBTDdsYJhhe9845p+dQ+kDbKRtiExoTGcsgZbYqny1wROCzBRTbutNzZvGr+gsHjCZTin0sNgOvcPtwGI9PbN14UF037UC8VXkD4lstdp5pU8womtftvyRVz4KJy4dlLoZqA+0VMtbniR0m2cCvxY7HXlmFqJTBh/qdjfX0iFhCqJVyx6/Zy5f+UjymZNJdJp+bZh8CLgF3lNcl9MwK1E5Oith54iCCweNS98owbbNCjYmbmZBjIt6pjyEjoqjC2iKV+mILGX5qnNOfrObxgpizdzpDMPBbuD2mlkaNCVvnGg5tny"
+  access_key = "ASIAT3EXCLIVMYZPIQ4O"
+  secret_key = "Kk52kb9bKFBDRfF4gpxDzbAjS0HWKE4TdsZj1jw0"
+  token      = "FwoGZXIvYXdzEL///////////wEaDHFCjcUGPfsma+GzsSLCAQdC7scI2yz1ntwVREH+59I565L980Jhb5xpdASCFzgCzE3QFJN8Hr78Mmmg6o0vi3pPNYpRg9+izHyibtNXMhRsAEUfExnCWZ2+G+8juNy7tkahtnsrYwto2ctNk7Fyvr6kGaw6xJkOjbYQ15jVbtKtVKNAynaK672Nq4ESzPu5ltmJMRvD6cXT3HUnS5zWLyuXpni5UPvcIYo8nbmuQcv2RVGKs0EMPUAimVmXy3Yf3cxQ2QvnPD/TMkHCvS5XjPnCKMrN3ZkGMi0tTxuAX+ZRP6AiHnoDyZZZHnmBDJOYicwLhzEppxTbo8Z/yczIUIOM3vKRI/M="
 }
 
 resource "aws_security_group" "security_gp" {
-  vpc_id = aws_vpc.my_vpc.id
+  vpc_id = "vpc-0c6fdbc545563b869"
 
   ingress {
     from_port        = 0
@@ -40,8 +40,9 @@ resource "aws_security_group" "security_gp" {
 resource "aws_instance" "instance1" {
   ami                    = "ami-0149b2da6ceec4bb0"
   instance_type          = "m4.large"
-  subnet_id              = aws_subnet.public_1.id
   vpc_security_group_ids = [aws_security_group.security_gp.id]
+  availability_zone      = "us-east-1c"
+  user_data              = file("userdata.sh")
   tags = {
     Name = "M4"
   }
@@ -50,8 +51,9 @@ resource "aws_instance" "instance1" {
 resource "aws_instance" "instance2" {
   ami                    = "ami-0149b2da6ceec4bb0"
   instance_type          = "m4.large"
-  subnet_id              = aws_subnet.public_1.id
   vpc_security_group_ids = [aws_security_group.security_gp.id]
+  availability_zone      = "us-east-1d"
+  user_data              = file("userdata.sh")
   tags = {
     Name = "M4"
   }
@@ -60,8 +62,9 @@ resource "aws_instance" "instance2" {
 resource "aws_instance" "instance3" {
   ami                    = "ami-0149b2da6ceec4bb0"
   instance_type          = "m4.large"
-  subnet_id              = aws_subnet.public_1.id
   vpc_security_group_ids = [aws_security_group.security_gp.id]
+  availability_zone      = "us-east-1c"
+  user_data              = file("userdata.sh")
   tags = {
     Name = "M4"
   }
@@ -70,8 +73,9 @@ resource "aws_instance" "instance3" {
 resource "aws_instance" "instance4" {
   ami                    = "ami-0149b2da6ceec4bb0"
   instance_type          = "m4.large"
-  subnet_id              = aws_subnet.public_1.id
   vpc_security_group_ids = [aws_security_group.security_gp.id]
+  availability_zone      = "us-east-1d"
+  user_data              = file("userdata.sh")
   tags = {
     Name = "M4"
   }
@@ -80,8 +84,9 @@ resource "aws_instance" "instance4" {
 resource "aws_instance" "instance5" {
   ami                    = "ami-0149b2da6ceec4bb0"
   instance_type          = "m4.large"
-  subnet_id              = aws_subnet.public_1.id
   vpc_security_group_ids = [aws_security_group.security_gp.id]
+  availability_zone      = "us-east-1c"
+  user_data              = file("userdata.sh")
   tags = {
     Name = "M4"
   }
@@ -90,8 +95,9 @@ resource "aws_instance" "instance5" {
 resource "aws_instance" "instance6" {
   ami                    = "ami-0149b2da6ceec4bb0"
   instance_type          = "t2.large"
-  subnet_id              = aws_subnet.public_1.id
   vpc_security_group_ids = [aws_security_group.security_gp.id]
+  availability_zone      = "us-east-1d"
+  user_data              = file("userdata.sh")
   tags = {
     Name = "T2"
   }
@@ -100,8 +106,9 @@ resource "aws_instance" "instance6" {
 resource "aws_instance" "instance7" {
   ami                    = "ami-0149b2da6ceec4bb0"
   instance_type          = "t2.large"
-  subnet_id              = aws_subnet.public_1.id
   vpc_security_group_ids = [aws_security_group.security_gp.id]
+  availability_zone      = "us-east-1d"
+  user_data              = file("userdata.sh")
   tags = {
     Name = "T2"
   }
@@ -110,8 +117,9 @@ resource "aws_instance" "instance7" {
 resource "aws_instance" "instance8" {
   ami                    = "ami-0149b2da6ceec4bb0"
   instance_type          = "t2.large"
-  subnet_id              = aws_subnet.public_1.id
   vpc_security_group_ids = [aws_security_group.security_gp.id]
+  availability_zone      = "us-east-1c"
+  user_data              = file("userdata.sh")
   tags = {
     Name = "T2"
   }
@@ -120,172 +128,140 @@ resource "aws_instance" "instance8" {
 resource "aws_instance" "instance9" {
   ami                    = "ami-0149b2da6ceec4bb0"
   instance_type          = "t2.large"
-  subnet_id              = aws_subnet.public_1.id
   vpc_security_group_ids = [aws_security_group.security_gp.id]
+  availability_zone      = "us-east-1d"
+  user_data              = file("userdata.sh")
   tags = {
     Name = "T2"
   }
 }
 
-resource "aws_instance" "instance10" {
-  ami                    = "ami-0149b2da6ceec4bb0"
-  instance_type          = "t2.large"
-  subnet_id              = aws_subnet.public_1.id
-  vpc_security_group_ids = [aws_security_group.security_gp.id]
-  tags = {
-    Name = "T2"
+data "aws_subnets" "all" {
+  filter {
+    name   = "vpc-id"
+    values = ["vpc-0c6fdbc545563b869"]
   }
 }
 
-
-#Network
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_alb" "load_balancer" {
+  name            = "load-balancer"
+  security_groups = [aws_security_group.security_gp.id]
+  subnets         = data.aws_subnets.all.ids
 }
 
-# Create an Internet Gateway.
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.my_vpc.id
-}
-
-resource "aws_subnet" "public_1" {
-  vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = true
-}
-
-# Create the second public subnet in the VPC for external traffic.
-resource "aws_subnet" "public_2" {
-  vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-east-1c"
-  map_public_ip_on_launch = true
-}
-
-
-
-resource "aws_lb" "load_balancer" {
-  name               = "load-balancer"
-  internal           = false
-  load_balancer_type = "application"
-  subnets            = [aws_subnet.public_1.id, aws_subnet.public_2.id]
-  security_groups    = [aws_security_group.security_gp.id]
-
-  enable_deletion_protection = true
-
-  tags = {
-    Environment = "production"
-  }
-}
-
-resource "aws_lb_target_group" "M4" {
+resource "aws_alb_target_group" "M4" {
   name     = "M4-instances"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = aws_vpc.my_vpc.id
+  vpc_id   = "vpc-0c6fdbc545563b869"
 }
 
-resource "aws_lb_target_group" "T2" {
+resource "aws_alb_target_group" "T2" {
   name     = "T2-instances"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = aws_vpc.my_vpc.id
+  vpc_id   = "vpc-0c6fdbc545563b869"
 }
 
 
-resource "aws_lb_listener" "listener" {
-  load_balancer_arn = aws_lb.load_balancer.arn
-  port              = "443"
+resource "aws_alb_listener" "listener" {
+  load_balancer_arn = aws_alb.load_balancer.arn
+  port              = 80
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.M4.arn
+    target_group_arn = aws_alb_target_group.M4.arn
   }
 }
 
-resource "aws_lb_listener_rule" "M4_rule" {
-  listener_arn = aws_lb_listener.listener.arn
+resource "aws_alb_listener_rule" "M4_rule" {
+  listener_arn = aws_alb_listener.listener.arn
 
   action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.M4.arn
+    target_group_arn = aws_alb_target_group.M4.arn
   }
 
   condition {
     path_pattern {
-      values = ["*/cluster1"]
+      values = ["/cluster1"]
     }
   }
 }
 
-resource "aws_lb_listener_rule" "T2_rule" {
-  listener_arn = aws_lb_listener.listener.arn
+resource "aws_alb_listener_rule" "T2_rule" {
+  listener_arn = aws_alb_listener.listener.arn
 
   action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.T2.arn
+    target_group_arn = aws_alb_target_group.T2.arn
   }
 
   condition {
     path_pattern {
-      values = ["*/cluster2"]
+      values = ["/cluster2"]
     }
   }
 }
 
 resource "aws_alb_target_group_attachment" "M4_attachment_1" {
-  target_group_arn = aws_lb_target_group.M4.arn
+  target_group_arn = aws_alb_target_group.M4.arn
   target_id        = aws_instance.instance1.id
+  port             = 80
 }
 
 resource "aws_alb_target_group_attachment" "M4_attachment_2" {
-  target_group_arn = aws_lb_target_group.M4.arn
+  target_group_arn = aws_alb_target_group.M4.arn
   target_id        = aws_instance.instance2.id
+  port             = 80
 }
 
 resource "aws_alb_target_group_attachment" "M4_attachment_3" {
-  target_group_arn = aws_lb_target_group.M4.arn
+  target_group_arn = aws_alb_target_group.M4.arn
   target_id        = aws_instance.instance3.id
+  port             = 80
+
 }
 
 resource "aws_alb_target_group_attachment" "M4_attachment_4" {
-  target_group_arn = aws_lb_target_group.M4.arn
+  target_group_arn = aws_alb_target_group.M4.arn
   target_id        = aws_instance.instance4.id
+  port             = 80
+
 }
 
 resource "aws_alb_target_group_attachment" "M4_attachment_5" {
-  target_group_arn = aws_lb_target_group.M4.arn
+  target_group_arn = aws_alb_target_group.M4.arn
   target_id        = aws_instance.instance5.id
+  port             = 80
 }
 
 resource "aws_alb_target_group_attachment" "T2_attachment_1" {
-  target_group_arn = aws_lb_target_group.T2.arn
+  target_group_arn = aws_alb_target_group.T2.arn
   target_id        = aws_instance.instance6.id
+  port             = 80
 }
 
 resource "aws_alb_target_group_attachment" "T2_attachment_2" {
-  target_group_arn = aws_lb_target_group.T2.arn
+  target_group_arn = aws_alb_target_group.T2.arn
   target_id        = aws_instance.instance7.id
+  port             = 80
 }
 
 resource "aws_alb_target_group_attachment" "T2_attachment_3" {
-  target_group_arn = aws_lb_target_group.T2.arn
+  target_group_arn = aws_alb_target_group.T2.arn
   target_id        = aws_instance.instance8.id
+  port             = 80
 }
 
 resource "aws_alb_target_group_attachment" "T2_attachment_4" {
-  target_group_arn = aws_lb_target_group.T2.arn
+  target_group_arn = aws_alb_target_group.T2.arn
   target_id        = aws_instance.instance9.id
-}
-
-resource "aws_alb_target_group_attachment" "T2_attachment_5" {
-  target_group_arn = aws_lb_target_group.T2.arn
-  target_id        = aws_instance.instance10.id
+  port             = 80
 }
 
 output "alb_dns_name" {
   description = "The Application Load Balancer DNS name"
-  value       = aws_lb.load_balancer.*.dns_name[0]
+  value       = aws_alb.load_balancer.*.dns_name[0]
 }
