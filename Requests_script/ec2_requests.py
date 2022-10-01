19299c15479562ed89e5ebaa32eca7b302d8a470
Anthony Sarkis <59975807+anthonysarkis@users.noreply.github.com>
2022-10-01 16:51:14 -0400

    added invert option to compare cluster benchmarks

--- Requests_script/ec2_requests.py
+++ Requests_script/ec2_requests.py
@@ -1,3 +1,4 @@
+
 import requests
 import threading
 import time
@@ -17,19 +18,28 @@ def thread2_requests(url, headers):
 
     for i in range(1000):
         requests.get(url, headers=headers)
+    
 
 
 if __name__ == '__main__':
     url = "http://" + os.environ['url']
     headers = {"content-type": "application/json"}
 
-    requests1 = threading.Thread(
-        target=thread1_requests, args=(url + "/cluster1", headers))
-    requests2 = threading.Thread(
-        target=thread2_requests, args=(url + "/cluster2", headers))
+    requests1 = threading.Thread(target=thread1_requests, args=(url + "/cluster1", headers))
+    requests2 = threading.Thread(target=thread2_requests, args=(url + "/cluster2", headers))
 
     requests1.start()
     requests2.start()
 
     requests1.join()
     requests2.join()
+
+    if os.environ['invert']:
+        requests1 = threading.Thread(target=thread1_requests, args=(url + "/cluster2", headers))
+        requests2 = threading.Thread(target=thread2_requests, args=(url + "/cluster1", headers))
+
+        requests1.start()
+        requests2.start()
+
+        requests1.join()
+        requests2.join()
