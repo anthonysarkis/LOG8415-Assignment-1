f792f88c4a69fbdf9003eaac2b720b764a5c5dee
WilliamYn <42267277+WilliamYn@users.noreply.github.com>
2022-10-04 19:53:38 -0400

    Used ALB DNS name in script.sh file

--- script.sh
+++ script.sh
@@ -2,3 +2,12 @@ export AWS_ACCESS_KEY_ID="$1"
 export AWS_SECRET_ACCESS_KEY="$2"
 export AWS_SESSION_TOKEN="$3"
 
+cd Terraform/
+
+terraform init
+
+terraform apply -auto-approve
+
+export url=$(terraform output --raw alb_dns_name)
+
+cd ..
