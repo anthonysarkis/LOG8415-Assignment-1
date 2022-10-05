5f7b8a1dab2c90baa3d3d9cc46c4c0388b9a6af7
WilliamYn <42267277+WilliamYn@users.noreply.github.com>
2022-10-04 22:48:21 -0400

    Documentation for sh file

--- script.sh
+++ script.sh
@@ -1,7 +1,15 @@
+################################
+# Setup AWS CLI
+################################
+
 export AWS_ACCESS_KEY_ID="$1"
 export AWS_SECRET_ACCESS_KEY="$2"
 export AWS_SESSION_TOKEN="$3"
 
+
+################################
+# Deploy Infra
+################################
 
 cd Terraform/
 
@@ -13,6 +21,16 @@ export url=$(terraform output --raw alb_dns_name)
 
 cd ..
 
+################################
+# Send Requests
+################################
+
 docker pull wayr/ec2_requests_slim:latest
 
 docker run -e url="$url" -e invert=False  wayr/ec2_requests_slim:latest
+
+################################
+# Get Benchmark
+################################
+
+# TODO
