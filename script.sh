59888560faf789cbe939a39f121167e50fa9ff28
WilliamYn <42267277+WilliamYn@users.noreply.github.com>
2022-10-04 22:47:08 -0400

    Dockerfile: added commands to run docker image

--- script.sh
+++ script.sh
@@ -2,6 +2,7 @@ export AWS_ACCESS_KEY_ID="$1"
 export AWS_SECRET_ACCESS_KEY="$2"
 export AWS_SESSION_TOKEN="$3"
 
+
 cd Terraform/
 
 terraform init
@@ -11,3 +12,7 @@ terraform apply -auto-approve
 export url=$(terraform output --raw alb_dns_name)
 
 cd ..
+
+docker pull wayr/ec2_requests_slim:latest
+
+docker run -e url="$url" -e invert=False  wayr/ec2_requests_slim:latest
