3f6698e091e043b8ac40f59739c137ca3762a1c7
royboutros <61989463+royboutros@users.noreply.github.com>
2022-10-05 22:28:12 -0400

    Fixed environment variables retrieval

--- Benchmark_script/ec2_benchmarks.py
+++ Benchmark_script/ec2_benchmarks.py
@@ -13,11 +13,16 @@ import os
 from datetime import timedelta, datetime
 from tabulate import tabulate
 
-client = boto3.client('cloudwatch', region_name="us-east-1")
+client = boto3.client('cloudwatch', 
+    region_name="us-east-1",
+    aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
+    aws_session_token=os.environ['AWS_SESSION_TOKEN']
+)
 
 def get_metric_data(metric, targetGroup1ARN, targetGroup2ARN, loadBalancer):
 
-    average = metric == 'TargetResponseTime' or metric == 'HealthyHostCount' or metric == 'UnHealthyHostCount'
+    average = metric == 'TargetResponseTime'
 
     response = client.get_metric_data(
         MetricDataQueries=[
@@ -61,7 +66,7 @@ def get_metric_data(metric, targetGroup1ARN, targetGroup2ARN, loadBalancer):
                         ]
                     },
                     'Period': 60,
-                    'Stat': 'Average' if average else 'Sum',
+                    'Stat': 'Average' if metric == 'TargetResponseTime' or metric == 'HealthyHostCount' or metric == 'UnHealthyHostCount' else 'Sum',
                     'Unit': 'Seconds' if metric == 'TargetResponseTime' else 'Count'
                 }
             },
@@ -237,9 +242,9 @@ if __name__ == '__main__':
         'HTTPCode_Target_2XX_Count',
         'HTTPCode_Target_4XX_Count',
         'HTTPCode_Target_5XX_Count'
-        # 'Connection Error Count',
-        # 'HealthyHostCount',
-        # 'UnHealthyHostCount'
+        'Connection Error Count',
+        'HealthyHostCount',
+        'UnHealthyHostCount'
     ]
     headers = [
         'Target',
@@ -248,9 +253,9 @@ if __name__ == '__main__':
         'Status 2XX',
         'Status 4XX',
         'Status 5xx'
-        # 'TargetConnectionErrorCount',
-        # 'HealthyHostCount',
-        # 'UnHealthyHostCount'
+        'TargetConnectionErrorCount',
+        'HealthyHostCount',
+        'UnHealthyHostCount'
     ]
     cluster1_data = ['Cluster1 (M4)']
     cluster2_data = ['Cluster2 (T2)']
@@ -258,7 +263,6 @@ if __name__ == '__main__':
 
     for i, metric in enumerate(metrics):
         response, average = get_metric_data(metric, cluster1, cluster2, load_balancer)
-        # print(response)
         cluster1_values = sum(response['MetricDataResults'][0]['Values'])
         cluster2_values = sum(response['MetricDataResults'][1]['Values'])
         total_values = (cluster1_values + cluster2_values) / 2 if average else cluster1_values + cluster2_values
@@ -270,4 +274,4 @@ if __name__ == '__main__':
     benchmark = tabulate([cluster1_data, cluster2_data, total_data], headers=headers)
     print(benchmark)
 
-    get_widgets()
+    # get_widgets()
