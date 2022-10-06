319456660b61360a558bf95590cc5efe879c3414
royboutros <61989463+royboutros@users.noreply.github.com>
2022-10-05 22:27:44 -0400

    Added environment variables for benchmark script

--- script.sh
+++ script.sh
@@ -21,8 +21,6 @@ export url=$(terraform output --raw alb_dns_name)
 export load_balancer=$(terraform output --raw load_balancer_arn_suffix)
 export cluster1=$(terraform output --raw M4_group_arn_suffix)
 export cluster2=$(terraform output --raw T2_group_arn_suffix)
-export M4=$(terraform output --raw M4)
-export T2=$(terraform output --raw T2)
 
 cd ..
 
@@ -40,6 +38,6 @@ docker run -e url="$url" -e invert=False  wayr/ec2_requests_slim:latest
 
 docker pull wayr/ec2_benchmarks:latest
 
-docker run -e load_balancer="$load_balancer" -e cluster1="$cluster1" -e cluster2="$cluster2" -e M4="$M4" -e T2="$T2" wayr/ec2_benchmarks:latest
+docker run -e AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" -e AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" -e AWS_SESSION_TOKEN="$AWS_SESSION_TOKEN" -e load_balancer="$load_balancer" -e cluster1="$cluster1" -e cluster2="$cluster2" wayr/ec2_benchmarks:latest
 
 $SHELL
