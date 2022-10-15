54715d0006269a0036ab93e3dd6e0914d13cfc41
MarcAntoine-AJ <38442879+MarcAntoine-AJ@users.noreply.github.com>
2022-10-15 00:55:51 -0400

    Changed requests to FuturesSession for async calls

--- Requests_script/ec2_requests.py
+++ Requests_script/ec2_requests.py
@@ -1,23 +1,26 @@
 
-import requests
+from requests_futures.sessions import FuturesSession
 import threading
 import time
 import os
 
 
 def thread1_requests(url, headers):
-    for i in range(1000):
-        requests.get(url, headers=headers)
+    session = FuturesSession()
+    for _ in range(1000):
+        session.get(url, headers=headers)
 
 
 def thread2_requests(url, headers):
-    for i in range(500):
-        requests.get(url, headers=headers)
+    session = FuturesSession()
+
+    for _ in range(500):
+        session.get(url, headers=headers)
 
     time.sleep(60)
 
-    for i in range(1000):
-        requests.get(url, headers=headers)
+    for _ in range(1000):
+        session.get(url, headers=headers)
     
 
 if __name__ == '__main__':
